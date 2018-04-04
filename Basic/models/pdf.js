a973ffe9126e5c6df4da3c0b63b7ca5708aca9fd
Daniel Ward <dward1502@gmail.com>
2018-04-04 15:42:36 -0700

    pdf layout complete

--- Basic/models/pdf.js
+++ Basic/models/pdf.js
@@ -1,148 +1,242 @@
+
+
 let inspectPDF = {
-        content: [
+   content: [
 
-            {
-                table: {
-                    widths: ['auto', '*'],
-                    body: [
-                        [
-                            {
-                                image: 'sampleImage.jpg', width: 150, height: 150
-                            },
-                            {
-                                width: '*',
-                                alignment: 'left',
-                                stack: [
+      {
+         table: {
+            widths: ['auto', '*'],
+            body: [
+               [
+                  {
+                     image: 'sampleImage.jpg', width: 150, height: 150
+                  },
+                  {
+                     width: '*',
+                     alignment: 'left',
+                     stack: [
+                        {
+                           style: 'header',
+                           text: 'Move In Inspection'
+                        },
+                        {
+                           style: 'street',
+                           text: '3668 Quimby St'
+                        },
+                        {
+                           style: 'city',
+                           text: 'San Diego, CA 92106'
+                        },
+                        {
+                           style: 'dateTable',
+                           table: {
+                              body: [
+                                 [
                                     {
-                                        style: 'header',
-                                        text: 'Move In Inspection'
+                                       border: [false, false, false, false],
+                                       fillColor: '#cccccc',
+                                       text: 'Inspected on April 3, 2018 3:31PM   |   Report Created on April 3, 2018 1:30 PM'
                                     },
-                                    {
-                                        style: 'street',
-                                        text: '3668 Quimby St'
-                                    },
-                                    {
-                                        style: 'city',
-                                        text: 'San Diego, CA 92106'
-                                    },
-                                    {
-                                        style: 'dateTable',
-                                        table: {
-                                            body: [
-                                                [
-                                                    {
-                                                        border: [false, false, false, false],
-                                                        fillColor: '#cccccc',
-                                                        text: 'Inspected on April 3, 2018 3:31PM   |   Report Created on April 3, 2018 1:30 PM'
-                                                    },
-                                                ]
-                                            ]
-                                        }
-                                    }
+                                 ]
+                              ]
+                           }
+                        }
 
-                                ]
-                            }
-                        ]
-                    ]
-                },
-                layout: {
-                    hLineWidth: function (line) { return line === 1; },
-                    vLineWidth: function () { return 0; },
-                    paddingBottom: function () { return 5; }
-                }
+                     ]
+                  }
+               ]
+            ]
+         },
+         layout: {
+            hLineWidth: function (line) { return line === 1; },
+            vLineWidth: function () { return 0; },
+            paddingBottom: function () { return 5; }
+         }
+      },
+      { text: 'Entry', style: 'section' },
+      {
+         style: 'sectionTable',
+         table: {
+            headerRows: 1,
+            body: [
+               [{ text: 'Items', style: 'dataHeader' }, { text: 'Clean', style: 'dataHeader' },
+               { text: 'Undamaged', style: 'dataHeader' }, { text: 'Working', style: 'dataHeader' },
+               { text: 'Notes', style: 'dataHeader' }],
+               ['Doors', 'No', 'Yes', 'Yes', 'There was dust and minor stains left on the wall'],
+               ['Walls', '', '', '', ''],
+               ['Ceiling', '', '', '', ''],
+               ['Flooring', '', '', '', ''],
+               ['Windows', '', '', '', ''],
+               ['Screens', '', '', '', ''],
+               ['Window Coverings', '', '', '', ''],
+            ]
+         }
+      },
+      { text: 'Entry Photos', style: 'photoText' },
+      {
+         table: {
+            widths: [125, 125, 125, 125],
+            body: [
+               [
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+               ],
+               [
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+               ]
+            ]
+         },
+         layout: 'noBorders'
+      },
+      {
+         style: 'line',
+         table: {
+            widths: ['*'],
+            body: [[" "], [" "]]
+         },
+         layout: {
+            hLineWidth: function (i, node) {
+               return (i === 0 || i === node.table.body.length) ? 0 : 2;
             },
-            { text: 'Entry', style: 'section' },
-            {
-                style: 'sectionTable',
-                table: {
-                    headerRows: 1,
-                    body: [
-                        [{ text: 'Items', style: 'dataHeader' }, { text: 'Clean', style: 'dataHeader' },
-                        { text: 'Undamaged', style: 'dataHeader' }, { text: 'Working', style: 'dataHeader' },
-                        { text: 'Notes', style: 'dataHeader' }],
-                        ['Doors', 'No', 'Yes', 'Yes', 'There was dust and minor stains left on the wall'],
-                        ['Walls', '', '', '', ''],
-                        ['Ceiling', '', '', '', ''],
-                        ['Flooring', '', '', '', ''],
-                        ['Windows', '', '', '', ''],
-                        ['Screens', '', '', '', ''],
-                        ['Window Coverings', '', '', '', ''],
-                    ]
-                }
+            vLineWidth: function (i, node) {
+               return 0;
             },
-            { text: 'Entry Photos', style: 'photoText' },
-            // {
-            //     table:{
-            //         body:[
-            //             [
-            //                 {
-            //                     rowSpan: 4,
-            //                     stack:[
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                         { image: 'sampleImage.jpg', margin: [20, 20, 20, 20] },
-            //                     ]
-            //                 }
-            //             ]
-            //         ]
-            //     }
-            // },        
-            {
-                table: {
-                    body: [
-                        [
-                            {
-                                border: [false, false, false, true],
-                                text: '                                                          '
-                            }
-                        ]
-                    ]
-                }
+         }
+      },
+      { text: 'Livingroom', style: 'section' },
+      {
+         style: 'sectionTable',
+         table: {
+            headerRows: 1,
+            body: [
+               [{ text: 'Items', style: 'dataHeader' }, { text: 'Clean', style: 'dataHeader' },
+               { text: 'Undamaged', style: 'dataHeader' }, { text: 'Working', style: 'dataHeader' },
+               { text: 'Notes', style: 'dataHeader' }],
+               ['Doors', 'No', 'Yes', 'Yes', 'There was dust and minor stains left on the wall'],
+               ['Walls', '', '', '', ''],
+               ['Ceiling', '', '', '', ''],
+               ['Flooring', '', '', '', ''],
+               ['Windows', '', '', '', ''],
+               ['Screens', '', '', '', ''],
+               ['Window Coverings', '', '', '', ''],
+            ]
+         }
+      },
+      { text: 'Livingroom Photos', style: 'photoText' },
+      {
+         table: {
+            widths: [125, 125, 125, 125],
+            body: [
+               [
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+               ],
+               [
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+                  {
+                     image: 'sampleImage.jpg', width: 125, height: 150
+                  },
+               ]
+            ]
+         },
+         layout: 'noBorders'
+      },
+      {
+         style: 'line',
+         table: {
+            widths: ['*'],
+            body: [[" "], [" "]]
+         },
+         layout: {
+            hLineWidth: function (i, node) {
+               return (i === 0 || i === node.table.body.length) ? 0 : 2;
             },
+            vLineWidth: function (i, node) {
+               return 0;
+            },
+         }
+      },
+   ],
+   styles: {
+      header: {
+         fontSize: 26,
+         bold: true,
+         margin: [5, 0, 0, 10],
+      },
+      street: {
+         fontSize: 14,
+         bold: true,
+         margin: [5, 35, 5, 5],
+      },
+      city: {
+         fontSize: 14,
+         bold: true,
+         margin: [5, 0, 5, 21],
+      },
+      dateTable: {
+         margin: [5, 0, 20, 15],
+         fontSize: 9
+      },
+      section: {
+         fontSize: 26,
+         bold: true,
+         margin: [0, 20, 5, 20]
+      },
+      dataHeader: {
+         fontSize: 14,
+         bold: true,
+         color: 'black',
+         fillColor: '#cccccc'
+      },
+      photoText: {
+         fontSize: 16,
+         bold: true,
+         margin: [0, 20, 15, 20]
+      },
+      line: {
+         margin: [0, 30, 0, 30]
+      }
+   },
+   defaultStyle: {
 
-        ],
-        styles: {
-            header: {
-                fontSize: 26,
-                bold: true,
-                margin: [5, 0, 0, 10],
-            },
-            street: {
-                fontSize: 14,
-                bold: true,
-                margin: [5, 35, 5, 5],
-            },
-            city: {
-                fontSize: 14,
-                bold: true,
-                margin: [5, 0, 5, 21],
-            },
-            dateTable: {
-                margin: [5, 0, 20, 15],
-                fontSize: 9
-            },
-            section: {
-                fontSize: 26,
-                bold: true,
-                margin: [0, 20, 5, 20]
-            },
-            dataHeader: {
-                fontSize: 14,
-                bold: true,
-                color: 'black',
-                fillColor: '#cccccc'
-            },
-            photoText: {
-                fontSize: 16,
-                bold: true,
-                margin: [0, 15, 15, 5]
-            }
-        },
-        defaultStyle: {
-
-        }
-    }
+   }
 }
